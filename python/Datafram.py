9a19494b69408faeda59e8f762173562ecf5d82d
yogesh1811 <yog.davv@gmail.com>
2018-07-19 18:00:03 +0400

    Update Datafram.py

--- python/Datafram.py
+++ python/Datafram.py
@@ -6,6 +6,5 @@ spark = SparkSession \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
-#df = spark.read.csv("C:\\Users\\s770322\\Desktop\\yourPathToCsv.csv", header=True)
 df = spark.read.csv("C:\\Yogi\\Notes\\learing\\spark\\olympix_data.csv", header=True)
-print(df.count())
+df.show()
